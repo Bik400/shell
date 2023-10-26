6231a9b8044c411d08a42920b8a69b7b2e275be5
Bikram Singh <bik@Bikrams-MacBook-Pro-2.local>
2023-10-26 14:49:27 -0700

    changed the parsing of the input

--- wish.c
+++ wish.c
@@ -1,29 +1,3 @@
-/* -- When a command is entered in the shell, a child process is called, executes
-the process, and then waits for more input
-
--- An interactive loop
--- Interactive mode and batch mode
--- In interactive mode a prompt is printed, and no prompt printed in batch mode
-
--- to parse inputs, strsep()
--- to execute commands, fork(), execv(), wait()/waitpid()
-
--- to implement cd, use chdir()
-
-1. break up the user input into tokens
-2. check which command to run and the path 
-
-to-do:
-- if there is an argument when there the file is run then run in batch mode 
-  else run in interactive mode
-- figure out what command is being called
-- if it's a built-in function then execute it without a child process
-- create a child process
-- execute the command that is entered
-- wait for the command to finish
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,40 +5,58 @@ to-do:
 #include <errno.h>
 
 int main(int argc, char* argv[]) {
-    size_t buffer_size = 32;
+    if (argc >= 2) {
+        // batch mode
+        printf("Batch mode\n");
+    } else {
+    size_t buffer_size = 0;
     char* buffer = NULL;
+    char* buffer_copy = NULL;
     ssize_t chars_read;
     char error_message[30] = "An error has occurred\n";
+    int count = 0;
+    char** input;
     while (1) {
         // prompt user input
         printf("wish> ");
         // get user input
         chars_read = getline(&buffer,&buffer_size,stdin);
+        buffer_copy = malloc(sizeof(char) * chars_read);
 
-        //split the input string 
-        char* token;
-        token = strtok(buffer, " ");
-        int i = 0;
-        char* input[30];
-        int count = 0;
-        // char* input = (char *) malloc(sizeof(char) * 10);
-        printf("Splitting the input into tokens\n");
-        while (token != NULL) {
-            input[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-            count++;
-        }
-        printf("Total commands inputted: %d\n", count);
-        input[i] = NULL;
-
-        // int input_size = sizeof(input) / sizeof(input[0]);
-        // check if user inputted ctrl+d or EOF
+         // check if user inputted ctrl+d or EOF
         if (chars_read == -1) {
             printf("Exiting the shell...");
             exit(0);
         }
 
+         if (buffer_copy == NULL) {
+            printf("Error allocating memory\n");
+            exit(1);
+        } else {
+            strcpy(buffer_copy, buffer);
+        }
+
+
+        char* token = strtok(buffer, " \n");
+        int num_of_tokens = 0;
+        int i = 0;
+        printf("Splitting the input into tokens\n");
+        while (token != NULL) {
+            num_of_tokens++;
+            token = strtok(NULL, " \n");
+            count++;
+        }
+        num_of_tokens++;
+        input = malloc(sizeof(char*) * num_of_tokens);
+        token = strtok(buffer_copy, " \n");
+
+        for (i = 0; token != NULL; i++) {
+            input[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(input[i], token);
+            token = strtok(NULL, " \n");
+        }
+        input[i] = NULL;
+        
         // check if the user inputted exit
         // error-check: check if an argument was passed when using exit
         if (strcmp(input[0], "exit\n") == 0) {
@@ -86,11 +78,10 @@ int main(int argc, char* argv[]) {
             write(STDERR_FILENO, error_message, strlen(error_message)); 
             printf("First error\n");
         } else if (strcmp(input[0], cd_command) == 0 && count == 2) {
-            if (chdir(input[1]) == -1) {
-                write(STDERR_FILENO, error_message, strlen(error_message));
-                printf("Second error\n");
-            } else {
+            if (chdir(input[1]) == 0) {
                 printf("Directory changed\n");
+            } else {
+                write(STDERR_FILENO, error_message, strlen(error_message));
             }
         }
 
@@ -98,30 +89,28 @@ int main(int argc, char* argv[]) {
     
 
         // execute a command
-        execv("/bin/ls", input);
-        perror("execv");
+        // execv("/bin/ls", input);
+        // perror("execv");
 
-
-
-
-        // char* command = input[0];
-        // char* full_path = strcat("/bin/", command);
-        // printf("path: %s\n", full_path);
-        // if (strcmp(command, "cd") != 0 && strcmp(command,"path") != 0) {
-        //     printf("command: %s\n",  command);
-        //     if (execv("/bin/ls", input) == -1) {
-        //         write(STDERR_FILENO, error_message, strlen(error_message)); 
-        //         printf("Third error\n");
-        //         printf("Error Number: %d\n", errno);
-        //     }
-        // } else {
-        //     printf("command was not run\n");
-        // }
+        char* command = input[0];
+        char* full_path = strcat("/bin/", command);
+        printf("path: %s\n", full_path);
+        if (strcmp(command, "cd") != 0 && strcmp(command,"path") != 0) {
+            printf("command: %s\n",  command);
+            if (execv(full_path, input) == -1) {
+                write(STDERR_FILENO, error_message, strlen(error_message)); 
+                printf("Third error\n");
+                printf("Error Number: %d\n", errno);
+            }
+        } else {
+            printf("command was not run\n");
+        }
         
        
 
 
         
+    }
     }
 
     return 0;
